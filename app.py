f56734d5135f7418821bae9a81d2a0562821fc0f
juliobarbagallo <julio.barbagallo@gmail.com>
2023-03-25 16:29:02 -0300

    feat: adds logic to calculate salary per minutes worked

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from payroll.payroll import Payroll
 
 if __name__ == "__main__":
     payroll = Payroll("input.txt")
-    payroll._calculate_pay()
+    payroll.calculate_pay()
+    for employee in payroll.employees:
+        print(f"The amount to pay to {employee.name} is: {employee.get_amount_to_earn()} USD")
+
--- payroll/payroll.py
+++ payroll/payroll.py
@@ -2,30 +2,45 @@ from employee.employee import Employee
 
 class Payroll:
     def __init__(self, file_name):
-        # self.employees = self.read_input_file(file_name)
-        self.employees = []
+        self.rates = {
+            "MO": [(0, 540, 25), (540, 1080, 15), (1080, 1440, 20)],
+            "TU": [(0, 540, 25), (540, 1080, 15), (1080, 1440, 20)],
+            "WE": [(0, 540, 25), (540, 1080, 15), (1080, 1440, 20)],
+            "TH": [(0, 540, 25), (540, 1080, 15), (1080, 1440, 20)],
+            "FR": [(0, 540, 25), (540, 1080, 15), (1080, 1440, 20)],
+            "SA": [(0, 540, 30), (540, 1080, 20), (1080, 1440, 25)],
+            "SU": [(0, 540, 30), (540, 1080, 20), (1080, 1440, 25)]
+        }
+        self.employees = self.read_input_file(file_name)
 
-
-    def _calculate_pay(self):
-        """
-        Calculates and prints the total amount to be paid to each employee based on their work schedule and hourly wage.
-
-        Reads the employee data from the input file provided during initialization, and for each employee, calculates the total
-        amount to be paid based on their work schedule and hourly wage. The results are printed to the console in the format
-        "The amount to pay {employee name} is: {total pay} USD".
-
-        Returns:
-            None
-        """
-        
-        #TODO: iterate by employees.
-        employee = {"name": "RENE", "schedule": "MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00"}
-        total_pay = 215
-        print(f"The amount to pay {employee['name']} is: {total_pay} USD")
+    def calculate_pay(self):
+        for employee in self.employees:
+            total_pay = 0
+            for day in employee.schedule:
+                for time in employee.schedule[day]:
+                    start_time, end_time = time.split("-")
+                    start_hour, start_minute = start_time.split(":")
+                    end_hour, end_minute = end_time.split(":")
+                    start_hour, start_minute, end_hour, end_minute = int(start_hour), int(start_minute), int(end_hour), int(end_minute)
+                    start_minutes = start_hour * 60 + start_minute
+                    end_minutes = end_hour * 60 + end_minute
+                    for rate_start, rate_end, rate in self.rates[day]:
+                        overlap_start = max(start_minutes, rate_start)
+                        overlap_end = min(end_minutes, rate_end)
+                        if overlap_start < overlap_end:
+                            total_pay += (overlap_end - overlap_start) / 60 * rate
+            employee.set_amount_to_earn(total_pay)
 
     def read_input_file(self, file_name):
         employees = []
         with open(file_name, "r") as f:
-            #TODO: process input file
-            pass
+            for line in f.readlines():
+                name, schedule_str = line.strip().split("=")
+                schedule = {}
+                for item in schedule_str.split(","):
+                    day, time = item[:2], item[2:]
+                    if day not in schedule:
+                        schedule[day] = []
+                    schedule[day].append(time)
+                employees.append(Employee(name, schedule))
         return employees
