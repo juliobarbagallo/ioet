87a9145c6c9f5066526523b08e00dcb031243b22
juliobarbagallo <julio.barbagallo@gmail.com>
2023-03-27 11:29:34 -0300

    feat: refactor the menu

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from payroll.payroll import Payroll
+from utils.utils import calculate_payroll, add_employee, export_payroll_report
 
-if __name__ == "__main__":
+if __name__ == "__main__":      
     payroll = Payroll("input.txt")
+    options = {
+        "1": lambda: calculate_payroll(payroll),
+        "2": lambda: add_employee(payroll),
+        "3": lambda: export_payroll_report(payroll),
+        "4": lambda: exit()
+    }
     while True:
         print("Options:")
         print("1) Generate payroll")
@@ -9,25 +16,7 @@ if __name__ == "__main__":
         print("3) Export to file")
         print("4) Exit")
         choice = input("Enter your choice: ")
-        if choice == "1":
-            payroll.calculate_pay()
-            for employee in payroll.employees:
-                print(f"The amount to pay to {employee.name} is: {employee.get_amount_to_earn()} USD")
-        elif choice == "2":
-            name = input("Enter the employee name: ")
-            schedule_str = input("Enter the employee schedule (format: MO09:00-17:00,TU09:00-17:00,...): ")
-            with open("input.txt", "a") as f:
-                f.write(f"\n{name}={schedule_str}")
-            print(f"Employee {name} added to input file")
-            payroll.employees = payroll.read_input_file("input.txt")
-        elif choice == "3":
-            payroll.calculate_pay()
-            output_file_name = input("Enter the output file name: ")
-            with open(output_file_name, "w") as f:
-                for employee in payroll.employees:
-                    f.write(f"The amount to pay to {employee.name} is: {employee.get_amount_to_earn()} USD\n")
-            print(f"Payroll exported to file {output_file_name}")
-        elif choice == "4":
-            break
-        else:
+        try:
+            options[choice]()
+        except KeyError:
             print("Invalid choice")
